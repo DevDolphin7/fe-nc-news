e7f33d5c2355b3abe3304ba5617f1cc6ae01a788
Dan Olver <devdolphin7@outlook.com>
2024-08-06 13:51:58 +0100

    add LoadComments function

--- src/hooks/Loading.jsx
+++ src/hooks/Loading.jsx
@@ -1,15 +1,15 @@
 import { useEffect, useState } from "react";
 import Lottie from "lottie-react";
-import { getArticles } from "../utils/api";
+import { getArticles, getComments } from "../utils/api";
 import LoadingDolphin from "../assets/LoadingDolphin.json";
 
-export function LoadArticles({ topic, sortArticlesBy, order }) {
+export function LoadArticles({ topic, sortArticlesBy, order, articleId = "" }) {
   const [articles, setArticles] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     setIsLoading(true);
-    getArticles({ topic, sortArticlesBy, order })
+    getArticles(topic, sortArticlesBy, order, articleId)
       .then((articles) => {
         setArticles(articles);
         setIsLoading(false);
@@ -20,6 +20,25 @@ export function LoadArticles({ topic, sortArticlesBy, order }) {
   }, []);
 
   return { isLoading, articles };
+}
+
+export function LoadComments(articleId) {
+  const [comments, setComments] = useState([]);
+  const [isLoadingComments, setIsLoadingComments] = useState(false);
+
+  useEffect(() => {
+    setIsLoadingComments(true);
+    getComments(articleId)
+      .then((articles) => {
+        setComments(articles);
+        setIsLoadingComments(false);
+      })
+      .catch((error) => {
+        alert(error);
+      });
+  }, []);
+
+  return { isLoadingComments, comments };
 }
 
 export function Loading() {
