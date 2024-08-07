d1fb470a7fd915e881e0da3c233d80dfa0641861
Dan Olver <devdolphin7@outlook.com>
2024-08-07 12:16:27 +0100

    Add functionallity to like a comment

--- src/components/CommentCard.jsx
+++ src/components/CommentCard.jsx
@@ -1,14 +1,33 @@
-import * as React from "react";
+import { useState } from "react";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
+import { patchVote } from "../utils/api";
 import Button from "@mui/material/Button";
 import LikeImage from "../assets/Like.png";
 
 export default function CommentCard({ comment }) {
-    const date = new Date(comment.created_at).toDateString();
+  const [commentOptimisticLike, setCommentOptimisticLike] = useState(0);
+  const date = new Date(comment.created_at).toDateString();
+
+  function handleCommentLike() {
+    if (commentOptimisticLike === 0) {
+      setCommentOptimisticLike(1);
+      patchVote(`/comments/${comment.comment_id}`, 1).catch((error) => {
+        setCommentOptimisticLike(0);
+        alert(error);
+      });
+    } else {
+      setCommentOptimisticLike(0);
+      patchVote(`/comments/${comment.comment_id}`, -1).catch((error) => {
+        setCommentOptimisticLike(1);
+        alert(error);
+      });
+    }
+  }
+
   return (
-    <Card sx={{ minWidth: 250 }}>
+    <Card sx={{ minWidth: 250, width: "88vw"}}>
       <CardContent>
         <Typography gutterBottom variant="h6" component="div">
           {comment.author}:
@@ -23,10 +42,10 @@ export default function CommentCard({ comment }) {
         >
           {date}
           <span>
-            <Button variant="text">
+            <Button variant="text" onClick={handleCommentLike}>
               <img src={LikeImage} height="25px" />
             </Button>
-            {comment.votes}
+            {comment.votes + commentOptimisticLike}
           </span>
         </Typography>
       </CardContent>
