b7bced7b308f9f1363fadb81bc9cf5130e8ccbba
Dan Olver <devdolphin7@outlook.com>
2024-08-07 14:54:02 +0100

    Apply logic to reload when required, sort comments to latest first

--- src/hooks/Loading.jsx
+++ src/hooks/Loading.jsx
@@ -3,7 +3,7 @@ import Lottie from "lottie-react";
 import { getArticles, getComments } from "../utils/api";
 import LoadingDolphin from "../assets/LoadingDolphin.json";
 
-export function LoadArticles({ topic, sortArticlesBy, order, articleId = ""}) {
+export function LoadArticles({ topic, sortArticlesBy, order, articleId = "" }, reload) {
   const [articles, setArticles] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
 
@@ -17,20 +17,21 @@ export function LoadArticles({ topic, sortArticlesBy, order, articleId = ""}) {
       .catch((error) => {
         alert(error);
       });
-  }, []);
+  }, [reload]);
 
   return { isLoading, articles };
 }
 
-export function LoadComments(articleId, {reload}) {
+export function LoadComments(articleId, reload) {
   const [comments, setComments] = useState([]);
   const [isLoadingComments, setIsLoadingComments] = useState(false);
 
   useEffect(() => {
     setIsLoadingComments(true);
     getComments(articleId)
-      .then((articles) => {
-        setComments(articles);
+      .then((comments) => {
+        comments.sort((a, b) => a.created_at < b.created_at)
+        setComments(comments);
         setIsLoadingComments(false);
       })
       .catch((error) => {
