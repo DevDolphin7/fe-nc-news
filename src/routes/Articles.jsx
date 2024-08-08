ed427880beeb7fa2aebcd5194fbb99a884ce5e93
Dan Olver <devdolphin7@outlook.com>
2024-08-08 16:24:19 +0100

    Update articles based on given topic

--- src/routes/Articles.jsx
+++ src/routes/Articles.jsx
@@ -1,18 +1,25 @@
-import { useState } from "react";
-import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Link, useSearchParams } from "react-router-dom";
 import { LoadArticles, Loading } from "../hooks/Loading";
 import ArticlesCard from "../components/ArticlesCard";
 import "./Articles.css";
 
 export default function Articles() {
-  const [topic, setTopic] = useState(undefined);
-  const [sortArticlesBy, setSortArticlesBy] = useState(undefined);
-  const [order, setOrder] = useState(undefined);
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [rerender, setRerender] = useState(false);
+  const topic = searchParams.get("topic");
+  const sortArticlesBy = searchParams.get("sortArticlesBy");
+  const order = searchParams.get("order");
+
+  useEffect(() => {
+    setRerender(!rerender);
+  }, [searchParams]);
 
   const { isLoading, articles } = LoadArticles({
     topic,
     sortArticlesBy,
     order,
+    rerender,
   });
 
   if (isLoading) {
