77fe5e2211e3a0e0b32edbec6ba8b7ef0b5ecedd
Dan Olver <devdolphin7@outlook.com>
2024-08-08 16:23:21 +0100

    Customise Dropdown menu. Code functional but will be refactored to maximise abstraction once NC tasks are complete.

--- src/components/Nav/Dropdown.jsx
+++ src/components/Nav/Dropdown.jsx
@@ -1,5 +1,13 @@
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useState, useRef, useEffect } from "react";
+import { CSSTransition } from "react-transition-group";
+import Button from "@mui/material/Button";
+import ClickAwayListener from "@mui/material/ClickAwayListener";
+import Grow from "@mui/material/Grow";
+import Paper from "@mui/material/Paper";
+import Popper from "@mui/material/Popper";
+import MenuItem from "@mui/material/MenuItem";
+import MenuList from "@mui/material/MenuList";
+import Stack from "@mui/material/Stack";
 import "./Dropdown.css";
 
 // What does it need to do? Show:
@@ -11,14 +19,17 @@ import "./Dropdown.css";
 //    < Sort
 //    < Order
 
-// How does it do it? An array of srtings and objects:
-// ["Welcome, user",
-//  "--divider--",
-//  {"< Articles": [{"Topic 1": callbackFn}, {"Topic 2": callbackFn}]},
-//  {Authors: callbackFn},
-//  "--divider--",
-//  {"< Sort": [{"author": "?sort_by=author"}]}
-//  ]
+// How WILL it do it AFTER NC TASK COMPLETION? An array of srtings and objects:
+// menu = [
+//   "Welcome User",
+//   "--divider--",
+//   { Articles: ["Here ok"] },
+//   {
+//     Authors: () => {
+//       console.log("Here in Authors callbackFn");
+//     },
+//   },
+// ];
 
 // Behaviour:
 // Any string is displayed (not a button)
@@ -33,33 +44,23 @@ import "./Dropdown.css";
 //    Any object key with a function value would result: {button: "key", select: callbackFn}
 //    Any object key with an array value would result: {button: "key", subMenu: [<parsed sub menu>]}
 
-export default function Dropdown(desiredMenu) {
-  const [dropdownOpen, setDropdownOpen] = useState(false);
-  const navigate = useNavigate();
-  desiredMenu = [
-    "Welcome User",
-    "--divider--",
-    { Articles: ["Here ok"] },
-    {
-      Authors: () => {
-        console.log("Here in Authors callbackFn");
-      },
-    },
-  ];
-  const menuItems = [
-    { text: "Welcome User" },
-    { text: "--divider--" },
-    { button: "Articles", subMenu: [{ text: "Here ok" }] },
-    {
-      button: "Authors",
-      select: () => {
-        return console.log("Here in Authors callbackFn");
-      },
-    },
-  ];
+// Dependencies:
+// React Transition Group `npm install react-transition-group --save`
+
+export default function Dropdown({ menuItems }) {
+  // Currently menu is not provided - to be updated after NC task completion
+  const [open, setOpen] = useState(false);
+  const anchorRef = useRef(null);
 
   const handleToggle = () => {
-    console.log("Hello");
+    setOpen((prevOpen) => !prevOpen);
+  };
+
+  const handleClose = (event) => {
+    if (anchorRef.current && anchorRef.current.contains(event.target)) {
+      return;
+    }
+    setOpen(false);
   };
 
   function handleListKeyDown(event) {
@@ -71,47 +72,155 @@ export default function Dropdown(desiredMenu) {
     }
   }
 
+  // return focus to the button when we transitioned from !open -> open
+  const prevOpen = useRef(open);
+  useEffect(() => {
+    if (prevOpen.current === true && open === false) {
+      anchorRef.current.focus();
+    }
+
+    prevOpen.current = open;
+  }, [open]);
+
   return (
-    <>
-      <button
-        onClick={() => setDropdownOpen(!dropdownOpen)}
-        className="toggle-dropdown"
+    <Stack>
+      <Button
+        ref={anchorRef}
+        id="composition-button"
+        aria-controls={open ? "composition-menu" : undefined}
+        aria-expanded={open ? "true" : undefined}
+        aria-haspopup="true"
+        onClick={handleToggle}
       >
-        <div className="button-div"></div>
-        <div className="button-div"></div>
-        <div className="button-div"></div>
-      </button>
-      {dropdownOpen && (
-        <DropdownMenu menuItems={menuItems} setDropdownOpen={setDropdownOpen} />
-      )}
-    </>
-    //onKeyDown=handleListKeyDown
+        <div></div>
+        <div></div>
+        <div></div>
+      </Button>
+      <Popper
+        open={open}
+        anchorEl={anchorRef.current}
+        role={undefined}
+        placement="bottom-start"
+        transition
+        disablePortal
+      >
+        {({ TransitionProps, placement }) => (
+          <Grow
+            {...TransitionProps}
+            style={{
+              transformOrigin:
+                placement === "bottom-start" ? "left top" : "left bottom",
+            }}
+          >
+            <Paper>
+              <ClickAwayListener onClickAway={handleClose}>
+                <MenuList
+                  autoFocusItem={open}
+                  id="composition-menu"
+                  aria-labelledby="composition-button"
+                  onKeyDown={handleListKeyDown}
+                >
+                  <DropdownMenu menuItems={menuItems} setOpen={setOpen} />
+                </MenuList>
+              </ClickAwayListener>
+            </Paper>
+          </Grow>
+        )}
+      </Popper>
+    </Stack>
   );
 }
 
-export function DropdownMenu({ menuItems, setDropdownOpen }) {
-  return (
-    <div className="dropdown-menu">
-      {menuItems.map((item, index) => {
+export function DropdownMenu({ menuItems, setOpen }, subMenu = []) {
+  const [activeMenu, setActiveMenu] = useState("main");
+  const [menuHeight, setMenuHeight] = useState(null);
+  const nodeRef = useRef(null);
+
+  function calculateHeight(domElement) {
+    const height = domElement.offsetHeight;
+    setMenuHeight(height)
+  }
+
+  const handledMenuItems = menuItems.map((item) => {
+    if (item.select) {
+      item.handleSelect = () => {
+        setOpen(false);
+        item.select();
+      };
+    }
+    // A lot of WET code! Below if statement and CSSTransitions will be
+    // refactored to recursive call - if time allows post core task completion
+    if (item.subMenu) {
+      item.subMenu.map((item) => {
         if (item.select) {
           item.handleSelect = () => {
-            setDropdownOpen(false);
+            setOpen(false);
             item.select();
           };
         }
-        return (
-          <DropdownItem
-            key={index}
-            // left={item.subMenu}
-            click={item.handleSelect}
-            textOnly={item.text}
-            setDropdownOpen={setDropdownOpen}
-          >
-            {item.button}
-          </DropdownItem>
-        );
-      })}
-    </div>
+      });
+    }
+    return item;
+  });
+
+  return (
+    <>
+      <div className="dropdown-menu" style={{height: menuHeight}}>
+        <CSSTransition
+          nodeRef={nodeRef}
+          in={activeMenu === "main"}
+          unmountOnExit
+          timeout={500}
+          onEnter={calculateHeight}
+          classNames="dropdown-menu-primary"
+        >
+          <div ref={nodeRef}>
+            {handledMenuItems.map((item, index) => {
+              if (item.subMenu) {
+                subMenu = item.subMenu;
+              }
+              return (
+                <DropdownItem
+                  key={index}
+                  click={item.handleSelect}
+                  textOnly={item.text}
+                  setOpen={setOpen}
+                  setActiveMenu={setActiveMenu}
+                >
+                  {item.button}
+                </DropdownItem>
+              );
+            })}
+          </div>
+        </CSSTransition>
+      </div>
+      <div className="dropdown-menu" style={{height: menuHeight}}>
+        <CSSTransition
+          nodeRef={nodeRef}
+          in={activeMenu === "sub"}
+          unmountOnExit
+          timeout={500}
+          onEnter={calculateHeight}
+          classNames="dropdown-menu-secondary"
+        >
+          <div ref={nodeRef}>
+            {subMenu.map((item, index) => {
+              return (
+                <DropdownItem
+                  key={index}
+                  click={item.handleSelect}
+                  textOnly={item.text}
+                  setOpen={setOpen}
+                  setActiveMenu={setActiveMenu}
+                >
+                  {item.button}
+                </DropdownItem>
+              );
+            })}
+          </div>
+        </CSSTransition>
+      </div>
+    </>
   );
 }
 
@@ -129,11 +238,13 @@ export function DropdownItem(props) {
     );
   }
   return (
-    <button onClick={props.click} className="dropdown-menu-item">
+    <button
+      onClick={() => props.setActiveMenu("sub")}
+      className="dropdown-menu-item"
+    >
       <span>
         <div className="sub-menu-div-upper"></div>
         <div className="sub-menu-div-lower"></div>
-        {props.left}
       </span>
       <span>{props.children}</span>
     </button>
